e803bbb02aa20260d653c90ded1537a69d1e595a
YutaSugimura <nextc3m@gmail.com>
2021-09-06 22:31:48 +0900

    add: tokens/optimism.json

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import Mainnet from './tokens/mainnet.json';
+import Optimism from './tokens/optimism.json';
 import type { EthereumTokenList } from './types';
 
 export const tokenList = (): EthereumTokenList => {
   return {
     mainnet: Mainnet,
+    optimism: Optimism,
   };
 };
