de2df9b8b1c0bb37b663fcb1f7352ad29b35f6d1
YutaSugimura <nextc3m@gmail.com>
2021-09-07 23:40:49 +0900

    add: tokens/polygon.json

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import Mainnet from './tokens/mainnet.json';
 import Optimism from './tokens/optimism.json';
+import Polygon from './tokens/polygon.json';
 import type { EthereumTokenList } from './types';
 
 export const tokenList = (): EthereumTokenList => {
   return {
     mainnet: Mainnet,
     optimism: Optimism,
+    polygon: Polygon,
   };
 };
